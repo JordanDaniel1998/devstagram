0a8999554a7ec18aa631556510643d4aa4be5f8b
Jordan Daniel <jordan.moreano.v@gmail.com>
2024-07-04 18:21:34 -0500

    add run build

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            /* input: [
+            input: [
                 "resources/css/app.css",
                 "resources/css/dropzone.css",
                 "resources/js/app.js",
                 "resources/js/dropzone.js",
                 "resources/js/alert.js",
-            ], */
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            ],
+            /* input: ["resources/css/app.css", "resources/js/app.js"], */
             refresh: true,
         }),
     ],
